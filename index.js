1d9b6c706b50aab73fb2eff3c2251b72e308e407
Hazrat Bilal <hazratbilal52521@gmail.com>
2025-02-19 17:54:01 +0500

    add vercel

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+// import express from "express";
+// import cors from "cors";
+// import authRouter from "./routes/admin.route.js";
+// import skRouter from "./routes/sk.route.js";
+// import wtRouter from "./routes/wt.route.js";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const port = 3000;
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.use("/api/auth", authRouter);
+// app.use("/api", skRouter);
+// app.use("/api", wtRouter);
+
+// app.get("/api/test", (req, res) => {
+//   res.send("ABC");
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
+
 import express from "express";
 import cors from "cors";
 import authRouter from "./routes/admin.route.js";
@@ -7,7 +34,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const port = 3000;
 const app = express();
 
 app.use(cors());
@@ -17,10 +43,8 @@ app.use("/api/auth", authRouter);
 app.use("/api", skRouter);
 app.use("/api", wtRouter);
 
-app.get("/test", (req, res) => {
-  res.send("ABC");
+app.get("/api/test", (req, res) => {
+  res.send("API is working!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+export default app;
