84793241b83449e5f48898a90274911bf9f805d8
Hazrat Bilal <hazratbilal52521@gmail.com>
2025-02-19 18:04:57 +0500

    testing

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-import express from "express";
-import cors from "cors";
-import authRouter from "./routes/admin.route.js";
-import skRouter from "./routes/sk.route.js";
-import wtRouter from "./routes/wt.route.js";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const port = 3000;
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/api/auth", authRouter);
-app.use("/api", skRouter);
-app.use("/api", wtRouter);
-
-app.get("/api/test", (req, res) => {
-  res.send("Test API working...");
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
-
 // import express from "express";
 // import cors from "cors";
 // import authRouter from "./routes/admin.route.js";
@@ -34,6 +7,7 @@ app.listen(port, () => {
 
 // dotenv.config();
 
+// const port = 3000;
 // const app = express();
 
 // app.use(cors());
@@ -44,7 +18,33 @@ app.listen(port, () => {
 // app.use("/api", wtRouter);
 
 // app.get("/api/test", (req, res) => {
-//   res.send("API is working!");
+//   res.send("Test API working...");
 // });
 
-// export default app;
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
+
+import express from "express";
+import cors from "cors";
+import authRouter from "./routes/admin.route.js";
+import skRouter from "./routes/sk.route.js";
+import wtRouter from "./routes/wt.route.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRouter);
+app.use("/api", skRouter);
+app.use("/api", wtRouter);
+
+app.get("/api/test", (req, res) => {
+  res.send("API is working!");
+});
+
+export default app;
